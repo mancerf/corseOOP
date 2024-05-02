3da732b97f73a63df403b69f31b0dca450f85923
Azat Yalmaev <azatyalmaev@gmail.com>
2024-05-02 15:53:07 +0300

    пиздец я заебался

--- 11.py
+++ 11.py
@@ -1,2 +1,9 @@
-a = [ 1,4,6]
-print([a[0],a[2]])
+
+a = [ 1,2,6,4,3]
+for i in range(len(a)):
+    if i >a[i]:
+        if i > 3:
+            print(1)
+            break
+    else:
+        print(65)
